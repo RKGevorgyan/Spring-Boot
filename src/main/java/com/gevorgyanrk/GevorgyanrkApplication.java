04c0d3cfd4277094d1800283dc394b9539f1e25b
robert_gevorgyan <gevorgyanrk@gmail.com>
2021-04-20 17:36:53 +0300

    Initial commit

---
+++ src/main/java/com/gevorgyanrk/GevorgyanrkApplication.java
@@ -0,0 +1,13 @@
+package com.gevorgyanrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GevorgyanrkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GevorgyanrkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gevorgyanrk/GevorgyanrkApplicationTests.java
@@ -0,0 +1,12 @@
+package com.gevorgyanrk;
+
+
+
+
+class GevorgyanrkApplicationTests {
+
+
+    void contextLoads() {
+    }
+
+}
